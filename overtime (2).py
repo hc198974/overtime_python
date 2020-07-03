f07fe62dd3cedc73559f793983b4f575829e09fd
hc198974 <hc198974@163.com>
2020-07-03 09:13:49 +0800

    窗体居中

--- overtime (2).py
+++ overtime (2).py
@@ -73,6 +73,31 @@ class Cwindow(object):
         self.name = '韩超'
         self.month = datetime.datetime.now().month-1
 
+    def set_win_center(self, root, curWidth='', curHight=''):
+        '''
+    设置窗口大小，并居中显示
+    param root:主窗体实例
+    param curWidth:窗口宽度，非必填，默认200
+    return:无
+    '''
+        if not curWidth:
+            '''获取窗口宽度，默认200'''
+            curWidth = root.winfo_width()
+        if not curHight:
+            '''获取窗口高度，默认200'''
+            curHight = root.winfo_height()
+
+        # 获取屏幕宽度和高度
+        scn_w, scn_h = root.maxsize()
+
+        # 计算中心坐标
+        cen_x = (scn_w - curWidth) / 2
+        cen_y = (scn_h - curHight) / 2
+
+        # 设置窗口初始大小和位置
+        size_xy = '%dx%d+%d+%d' % (curWidth, curHight, cen_x, cen_y)
+        root.geometry(size_xy)
+
     def askname(self):
         # 获取字符串（标题，提示，初始值）
         name = tkinter.simpledialog.askstring(
@@ -93,7 +118,10 @@ class Cwindow(object):
         # 创建主窗口
         root = tkinter.Tk()
         # 设置窗口大小
-        root.minsize(300, 150)
+        root.resizable(False, False)
+        root.title('加班')
+        root.update()
+        self.set_win_center(root, 300, 150)
         # 添加按钮
         btn1 = tkinter.Button(root, text='获取用户名', command=self.askname)
         btn1.pack(expand='yes')
